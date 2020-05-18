b057fe87a7b8e5324d4ef83e861729a9e5cbae82
Alionides <Alionides>
2020-05-18 11:56:42 +0400

    first repo

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index(){
+        return view('site.index');
+    }
+    public function post(){
+        return view('site.post');
+    }
+}
